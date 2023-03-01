1a0e1a218bc73317b592657778846b6e266c4e1a
Phillip Yagyu <107158459+phillyagyu@users.noreply.github.com>
2023-02-28 23:25:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// Filtering by name
+const filterElement = document.querySelector('header input')
+const cards = document.querySelectorAll('.cards .card')
+
+filterElement.addEventListener('input', filterCards)
+
+function filterCards() {
+  if(filterElement.value != '') {
+    for(let card of cards) {
+      let title = card.querySelector('h3')
+      title = title.textContent.toLowerCase()
+      let filterText = filterElement.value.toLowerCase()
+      if(!title.includes(filterText)) {
+        card.style.display = 'none'
+      } else {
+        card.style.display = 'block'
+      }
+    }
+  } else {
+    for(let card of cards) {
+      card.style.display = 'block'
+    }
+  }
+}
+
+// Filtering by city
+const filterSelect = document.querySelector('header select')
+const optionsCache = document.querySelectorAll('select option')
+
+filterSelect.addEventListener('click', filterCardsBySelect)
+
+function filterCardsBySelect() {
+  let citySelected = filterSelect.value
+  for (let card of cards) {
+    let cityId = card.getElementsByClassName('location')[0].id
+    if(citySelected != 0) {
+      if(cityId === citySelected) {
+        card.style.display = 'block'
+      } else {
+        card.style.display = 'none'
+      }
+    } else {
+      for(let card of cards) {
+        card.style.display = 'block'
+      }
+    }
+  }
+}
+
+
+// function filterCardsBySelect() {
+//   console.log(filterSelect.value)
+// }
+
+
+// Adding no submit event
+const form = document.querySelector('form')
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+})
